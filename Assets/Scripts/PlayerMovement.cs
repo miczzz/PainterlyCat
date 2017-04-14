35d1db7881eb3df373bc67b070304e1b5d4cb227
miczzz <mziesemann@gmail.com>
2017-04-14 13:45:03 +0200

    Unity project started
    cat imported, first movement script implemented

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public int velocity = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * velocity;
+        float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * velocity;
+
+        transform.Translate(horizontalMovement, 0, verticalMovement);
+        
+    }
+}

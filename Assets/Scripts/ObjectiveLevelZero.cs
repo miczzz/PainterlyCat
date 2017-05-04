1f0185fc870a2926a551b14aeae7c14124d61249
miczzz <mziesemann@gmail.com>
2017-05-04 15:01:03 +0200

    Level Zero
    Testlevel eingebaut, das die Spielmechanik verdeutlichen sollte

---
+++ Assets/Scripts/ObjectiveLevelZero.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Easy peasy lemon squeezy level zum ausprobieren
+public class ObjectiveLevelZero : MonoBehaviour {
+
+    public GameObject object1;
+    public GameObject object2;
+    public GameObject object3;
+
+    public Material object1WantedColor;
+    public Material object2WantedColor;
+    public Material object3WantedColor;
+
+    private Material obj1Color;
+    private Material obj2Color;
+    private Material obj3Color;
+
+    private bool levelZeroComplete;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!levelZeroComplete) {
+
+            obj1Color = object1.GetComponent<Renderer>().material;
+            obj2Color = object2.GetComponent<Renderer>().material;
+            obj3Color = object3.GetComponent<Renderer>().material;
+
+            Debug.Log(obj1Color.color == object1WantedColor.color);
+            Debug.Log(obj2Color.color == object2WantedColor.color);
+            Debug.Log(obj3Color.color == object3WantedColor.color);
+
+            if (obj1Color.color == object1WantedColor.color &&
+                obj2Color.color == object2WantedColor.color &&
+                obj3Color.color == object3WantedColor.color)
+            {
+                Debug.Log("Level complete");
+                levelZeroComplete = true;
+            }
+    
+        }
+
+
+	}
+}

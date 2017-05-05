fe71dbc7e98690f83b06d65054a5e79e7689e956
Anh Thu Nguyen <anhthu@onlinehome.de>
2017-05-05 13:41:03 +0200

    create second version of player movement

---
+++ Assets/Scripts/Player/PlayerMovementScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+	public float speed = 10.0F;
+
+	// Use this for initialization
+	void Start () {
+		// make cursor invisble and cursor cant go outside screen
+		//Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float vertical = Input.GetAxis ("Vertical") * speed;
+		float horizonzal = Input.GetAxis ("Horizontal") * speed;
+		vertical *= Time.deltaTime;
+		horizonzal *= Time.deltaTime;
+
+		transform.Translate (horizonzal, 0, vertical);
+
+		/*
+		if (Input.GetKeyDown ("escape")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+		*/
+
+	}
+}

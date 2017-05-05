772956d3226bea8f3661bf863a5265bec655a9c7
Anh Thu Nguyen <anhthu@onlinehome.de>
2017-05-05 23:15:52 +0200

    create more color materials

---
+++ Assets/Scripts/Cube/CubeColorChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColorChange : MonoBehaviour {
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		rend.enabled = true;
+	}
+	
+	public void changeColor(Material material) {
+		rend.sharedMaterial = material;
+	}
+}
---
+++ Assets/Scripts/Cube/PaintPotScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintPotScript : MonoBehaviour {
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		rend.enabled = true;
+	}
+	
+	// Update is called once per frame
+	public Material getColor() {
+		if (rend.material.name != null) {
+			return rend.material;
+		}
+		Debug.Log ("Paint Pot has no color!");
+		return null;
+	}
+}

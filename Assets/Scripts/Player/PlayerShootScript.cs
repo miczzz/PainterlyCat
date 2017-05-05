7210e3e34ecbd733dce6147b576d2158f3eff523
Anh Thu Nguyen <anhthu@onlinehome.de>
2017-05-05 17:38:57 +0200

    first attempt of player shooting

---
+++ Assets/Scripts/Player/PlayerShootScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootScript : MonoBehaviour {
+
+	private int cooldown;
+	public int maxCooldown = 100;
+
+	public GameObject startPosition;
+
+	public GameObject projectile;     
+
+	// Use this for initialization
+	void Start () {
+		cooldown = maxCooldown;
+	}
+
+	public void FixedUpdate() {
+		
+		cooldown--;
+
+		if (Input.GetButtonDown ("Fire1")) {
+			if (cooldown <= 0) {
+				GameObject bullet = Instantiate (projectile);
+				bullet.transform.position = startPosition.transform.position;
+				cooldown = maxCooldown;
+			}
+		}
+	
+	}
+}

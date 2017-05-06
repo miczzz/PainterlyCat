c43840a884e520d196a1289debd7b556ce4b21ca
Anh Thu Nguyen <anhthu@onlinehome.de>
2017-05-06 13:10:51 +0200

    create door and task script

--- Assets/Scripts/Player/PlayerShootScript.cs
+++ Assets/Scripts/Player/PlayerShootScript.cs
@@ -40,42 +40,42 @@ public class PlayerShootScript : MonoBehaviour {
 			// Start ShotEffect coroutine to turn our laser line on and off
 			StartCoroutine (ActivateLaser());
 
-			// Create a vector at the center of our camera's viewport
-			Vector3 rayOrigin = cam.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
+			checkIfLaserHitSomething ();
+		}
+	}
 
-			// Declare a raycast hit to store information about what our raycast has hit
-			RaycastHit hit;
+	private void checkIfLaserHitSomething() {
+		// Create a vector at the center of our camera's viewport
+		Vector3 rayOrigin = cam.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
+		// Declare a raycast hit to store information about what our raycast has hit
+		RaycastHit hit;
 
-			// set startposition for laser
-			laserLine.SetPosition (0, startPosition.position);
+		// set startposition for laser
+		laserLine.SetPosition (0, startPosition.position);
+		if (Physics.Raycast (rayOrigin, cam.transform.forward, out hit, laserRange))
+		{
+			// Set the end position for laser 
+			laserLine.SetPosition (1, hit.point);
 
-			// Check if our raycast has hit anything
-			if (Physics.Raycast (rayOrigin, cam.transform.forward, out hit, laserRange))
-			{
-				// Set the end position for our laser line 
-				laserLine.SetPosition (1, hit.point);
-
-				PaintPotScript paintPot = hit.collider.GetComponent<PaintPotScript> ();
-				if (paintPot != null) {
-					if (paintPot.getColor () != null) {
-						laserColor = paintPot.getColor ();
-						laserLine.sharedMaterial = laserColor;
-					}
-				} 
-				else {
-					CubeColorChange colorChange = hit.collider.GetComponent<CubeColorChange> ();
-					if (colorChange != null) {
-						colorChange.changeColor (laserColor);
-					}
+			PaintPotScript paintPot = hit.collider.GetComponent<PaintPotScript> ();
+			if (paintPot != null) {
+				if (paintPot.getColor () != null) {
+					laserColor = paintPot.getColor ();
+					laserLine.sharedMaterial = laserColor;
 				}
-						
-
-				/*
+			} 
+			else {
+				CubeColorChange colorChange = hit.collider.GetComponent<CubeColorChange> ();
+				if (colorChange != null) {
+					colorChange.changeColor (laserColor);
+				}
+			}
+			/*
 				// Get a reference to a health script attached to the collider we hit
-				ShootableBox health = hit.collider.GetComponent<ShootableBox>();
-				*/
-				// If there was a health script attached
-				/*
+			ShootableBox health = hit.collider.GetComponent<ShootableBox>();
+			*/
+			// If there was a health script attached
+			/*
 				if (health != null)
 				{
 					// Call the damage function of that script, passing in our gunDamage variable
@@ -83,34 +83,28 @@ public class PlayerShootScript : MonoBehaviour {
 				}
 				*/
 
-				// Check if the object we hit has a rigidbody attached
-				/*
+			// Check if the object we hit has a rigidbody attached
+			/*
 				if (hit.rigidbody != null)
 				{
 					// Add force to the rigidbody we hit, in the direction from which it was hit
 					hit.rigidbody.AddForce (-hit.normal * hitForce);
 				}
 				*/
-			}
-			else
-			{
-				// If we did not hit anything, set the end of the line to a position directly in front of the camera at the distance of weaponRange
-				laserLine.SetPosition (1, rayOrigin + (cam.transform.forward * laserRange));
-			}
+		}
+		else
+		{
+			// If not hit anything, set end of laser to position directly in front of camera at distance of laserRange
+			laserLine.SetPosition (1, rayOrigin + (cam.transform.forward * laserRange));
 		}
 	}
 		
 
-	private IEnumerator ActivateLaser()
-	{
+	private IEnumerator ActivateLaser() {
 
-		// Turn on line renderer
 		laserLine.enabled = true;
-
 		//Wait for 1 second
 		yield return shotDuration;
-
-		// Deactivate line renderer after waiting
 		laserLine.enabled = false;
 	}
 }
---
+++ Assets/Scripts/Room/TaskScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskScript : MonoBehaviour {
+
+	public GameObject object1;
+	public Material object1WantedColor;
+	private Material object1CurrentColor;
+
+	public GameObject door;
+
+	private bool levelComplete = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!levelComplete) {
+
+			object1CurrentColor = object1.GetComponent<Renderer> ().material;
+
+			if (object1WantedColor.color == object1CurrentColor.color) {
+				Debug.Log ("Level complete");
+				levelComplete = true;
+
+				Destroy (door);
+			}
+		}
+	}
+}

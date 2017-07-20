480a52319b409c1b5991563ccbed41aa4cc09b01
Anh Thu Nguyen <anhthu@onlinehome.de>
2017-07-20 14:01:30 +0200

    create main menu and connect first levels

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas MainMenu;
+
+	public Canvas HelpMenu;
+
+	public void Awake() {
+	
+		HelpMenu.enabled = false;
+
+	}
+
+	public void OpenHelpScreen() {
+	
+		HelpMenu.enabled = true;
+		MainMenu.enabled = false;
+	}
+
+	public void ReturnToMainScreen() {
+	
+		HelpMenu.enabled = false;
+		MainMenu.enabled = true;
+	}
+
+
+	public void LoadLevelOne() {
+		SceneManager.LoadScene (2);
+	}
+
+	public void LoadLevelMultiplayer() {
+		SceneManager.LoadScene (1);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+
+}

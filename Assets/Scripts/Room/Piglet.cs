71a9664df7471c03fafafe0e3e98990c4cf5fbb3
miczzz <mziesemann@gmail.com>
2017-07-26 16:28:30 +0200

    Easy Pattern Level
    because the other one is not a good introduction

---
+++ Assets/Scripts/Room/Piglet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piglet : MonoBehaviour {
+
+    public GameObject dialogueBox;
+    public Transform target;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+
+        // vielleicht noch hinzufügen, dass das Schwein die Katze anschaut, mal schauen
+   //     transform.parent.LookAt(target);
+        dialogueBox.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        
+        dialogueBox.SetActive(false);
+    }
+
+}

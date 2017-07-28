4575475da3eb3b8ec7e3ee68dc43d99b46e6ab39
miczzz <mziesemann@gmail.com>
2017-07-28 13:49:39 +0200

    Documentation
    and some bugfixes

---
+++ Assets/Scripts/Room/Sonne.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Sonne : MonoBehaviour {
+
+    public Material sonnenFarbe;
+    public GameObject licht;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.GetComponent<Renderer>().material.color == sonnenFarbe.color)
+        {
+            licht.SetActive(true);
+        }
+
+    }
+}
